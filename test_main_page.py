3fc65e194000684112d8263213cd11c5ae75fbaf
Yuliya Fomenko <y.fomenko14@gmail.com>
2024-10-24 10:14:28 +0300

    Разделение в разные методы открытие страницы и логина; создание файлов page object для главной и базовой страниц; добавление в base_page класса для открытия браузера

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
 import time
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    # time.sleep(10)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
-    # time.sleep(10)
+    time.sleep(10)
+
+def test_guest_can_go_to_login_page(browser): 
+   browser.get(link)
+   time.sleep(10)
+   go_to_login_page(browser)
