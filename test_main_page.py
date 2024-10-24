0d0bf2995e93f6dfa51b42356bf8f7eec16a6781
Yuliya Fomenko <y.fomenko14@gmail.com>
2024-10-24 10:46:18 +0300

    Добавление метода для клика по кнопке логин в main_page; редактирование test_main_page в соответствии с page object

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,9 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 import time
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
     time.sleep(10)
-
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link)
-   time.sleep(10)
-   go_to_login_page(browser)
+    page.go_to_login_page()
