1139b01779ca0c59871d767679d43aae0e683f5f
Yuliya Fomenko <y.fomenko14@gmail.com>
2024-10-28 10:14:53 +0300

    Правки ассертов в файле login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,16 +8,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        "login" in self.browser.current_url
-        assert True, "URL form is not valid"
+        assert "login" in self.browser.current_url, "URL form is not valid"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        self.is_element_present(*LoginPageLocators.LOGIN_FORM)
-        assert True, "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        self.is_element_present(*LoginPageLocators.REGISTRATION_FORM)
-        assert True, "Registration form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
