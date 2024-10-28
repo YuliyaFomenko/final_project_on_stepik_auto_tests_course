b95a7bae5e2c0fccf30556720df45e7f96be6076
Yuliya Fomenko <y.fomenko14@gmail.com>
2024-10-28 08:19:05 +0300

    Перенос локатора для кнопки логина в locators.py; реализация проерок для страницы логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,12 @@
-from .pages.main_page import MainPage
 import time
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    time.sleep(10)
-    # page.go_to_login_page()
+    time.sleep(5)
     page.should_be_login_link()
+    time.sleep(5)
+    page.go_to_login_page()
+    time.sleep(5)
